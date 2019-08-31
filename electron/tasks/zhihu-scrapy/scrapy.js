e5a0de6edf9a91dd47b9b40d2b9ae99a28b6670e
weixiaoyi <2278095567@qq.com>
2019-09-01 00:35:34 +0800

    注入

--- electron/tasks/zhihu-scrapy/scrapy.js
+++ electron/tasks/zhihu-scrapy/scrapy.js
@@ -6,27 +6,47 @@ const buttonStyle = {
   color: "white"
 };
 
+const detailStyle = {
+  background: "green",
+  padding: "5px",
+  color: "white"
+};
+
 window.ipc.on("createUtils", () => {
   const div = document.createElement("div");
-  const content = `<div style=${formatStyle({
+  const content = `
+<div style=${formatStyle({
     position: "fixed",
     top: "0",
     background: "rgba(0,0,0,.6)",
     "z-index": "10000"
-  })}><button style=${formatStyle(
-    buttonStyle
-  )}  id="start" >开始</button><button id="end" style=${formatStyle(
-    buttonStyle
-  )}>结束</button><button id="seeUrl" style=${formatStyle(
-    buttonStyle
-  )}>查看url</button></div>`;
+  })}>
+<button style=${formatStyle(buttonStyle)}  id="start" >开始滚动</button>
+<button id="end" style=${formatStyle(buttonStyle)}>结束滚动</button>
+<button id="seeUrl" style=${formatStyle(buttonStyle)}>查看url</button>
+<button id="seeAnswers" style=${formatStyle(buttonStyle)}>查看答案列表</button>
+<div>
+<div id="totalAnswers" style=${formatStyle(detailStyle)}></div>
+<div id="haveGetAnswers" style=${formatStyle(detailStyle)}></div>
+</div>
+</div>
+`;
   div.innerHTML = `${content}`;
   document.body.appendChild(div);
   document.getElementById("start").onclick = () => {
+    const ansers = document.getElementsByClassName("List-headerText")[0]
+      .innerHTML;
+    const total = document.getElementById("totalAnswers");
+    const haveGet = document.getElementById("haveGetAnswers");
+    total.innerHTML = `总共：${ansers}`;
     clearInterval(window.interval);
     window.interval = setInterval(() => {
       const t = document.body.clientHeight;
       window.scrollTo({ top: t, left: 0, behavior: "smooth" });
+      const len = document
+        .getElementsByClassName("Card AnswersNavWrapper")[0]
+        .getElementsByClassName("List-item").length;
+      haveGet.innerHTML = `已经获取${len}个答案`;
     }, 3000);
   };
   document.getElementById("end").onclick = () => {
@@ -34,5 +54,9 @@ window.ipc.on("createUtils", () => {
   };
   document.getElementById("seeUrl").onclick = () => {
     alert(document.location.href);
+  };
+  document.getElementById("seeAnswers").onclick = () => {
+    const len = document.getElementsByClassName("List-item").length;
+    alert(len);
   };
 });
