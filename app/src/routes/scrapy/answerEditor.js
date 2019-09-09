992133a6f2f3a10cf63bcfb2afff2eb94c017104
weixiaoyi <2278095567@qq.com>
2019-09-09 22:24:12 +0800

    分离answerEditor

---
+++ app/src/routes/scrapy/answerEditor.js
@@ -0,0 +1,180 @@
+import React, { Component } from "react";
+import { Button, Card, Divider, Select, Input, Popover, Form } from "antd";
+import { Editor } from "../../components";
+import { Inject, formatTime } from "../../utils";
+import * as styles from "./index.module.scss";
+
+const { TextArea } = Input;
+
+@Form.create()
+@Inject(({ scrapyStore: model }) => ({
+  model
+}))
+class AnswerEditor extends Component {
+  state = {
+    editable: null,
+    showUserDownload: false
+  };
+
+  componentDidMount() {
+    window.ipc &&
+      window.ipc.on("scrapy.download-image", (e, arg) => {
+        if (!arg) return;
+        const images = document
+          .getElementById("answer-operation")
+          .querySelectorAll("img");
+        Array.prototype.map.call(images, item => {
+          const src = item.getAttribute("src");
+          item.setAttribute("src", `${src}?time=${Date.now()}`);
+        });
+      });
+  }
+
+  updateAnswer = ({ answerId, ...rest }) => {
+    const {
+      model: { dispatch }
+    } = this.props;
+    dispatch({
+      type: "ipc-update-answer",
+      payload: {
+        answerId,
+        ...rest
+      }
+    });
+  };
+
+  render() {
+    const { editable, showUserDownload } = this.state;
+    const { selectOne } = this.props;
+
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div>
+        {selectOne.title && (
+          <div className={styles.contentHeader}>
+            <span className={styles.title}>{selectOne.title}</span>
+            <div className={styles.info}>
+              {selectOne.createTime && (
+                <span className={styles.createTime}>
+                  {formatTime(selectOne.createTime)}
+                </span>
+              )}
+              <span className={styles.author}>({selectOne.authorName})</span>
+            </div>
+          </div>
+        )}
+
+        <Editor
+          className={styles.editorContent}
+          content={selectOne.content}
+          editable={editable}
+        >
+          {editor => (
+            <div className={styles.utils}>
+              <div>
+                <Button
+                  disabled={!editable}
+                  type="primary"
+                  onClick={() => {
+                    this.updateAnswer({
+                      answerId: selectOne.answerId,
+                      content: editor.txt.html()
+                    });
+                  }}
+                >
+                  保存
+                </Button>
+                <Divider type="vertical" />
+                <Button
+                  disabled={editable}
+                  type="primary"
+                  onClick={() => {
+                    this.setState({
+                      editable: true
+                    });
+                  }}
+                >
+                  编辑
+                </Button>
+              </div>
+              <div>
+                <Popover
+                  visible={showUserDownload}
+                  getPopupContainer={() =>
+                    document.getElementById("user_download")
+                  }
+                  content={
+                    <div style={{ width: 400 }}>
+                      <Form
+                        onSubmit={e => {
+                          e.preventDefault();
+                          this.props.form.validateFields((err, values) => {
+                            if (!err) {
+                              const { filename, dataUrl } = values;
+                              console.log(values);
+                              // dispatch({
+                              //   type: "ipc-download-image",
+                              //   payload: {
+                              //     dataUrl,
+                              //     filename
+                              //   }
+                              // });
+                            }
+                          });
+                        }}
+                        className="login-form"
+                        {...{
+                          labelCol: {
+                            sm: { span: 5 }
+                          },
+                          wrapperCol: {
+                            sm: { span: 19 }
+                          }
+                        }}
+                      >
+                        <Form.Item label="filename">
+                          {getFieldDecorator("filename", {
+                            rules: [{ required: true, message: "required" }]
+                          })(<Input placeholder="filename" />)}
+                        </Form.Item>
+                        <Form.Item label="dataUrl">
+                          {getFieldDecorator("dataUrl", {
+                            rules: [{ required: true, message: "required" }]
+                          })(<TextArea placeholder="dataUrl" />)}
+                        </Form.Item>
+                        <Form.Item
+                          className={styles.nolabel}
+                          label={<span style={{ after: "unset" }}>&nbsp;</span>}
+                        >
+                          <Button type="primary" htmlType="submit">
+                            确认
+                          </Button>
+                        </Form.Item>
+                      </Form>
+                    </div>
+                  }
+                  title="手动添加图片"
+                  trigger="click"
+                >
+                  <Button
+                    id="user_download"
+                    onClick={() => {
+                      this.setState({
+                        showUserDownload: !showUserDownload
+                      });
+                    }}
+                  >
+                    手动下载图片
+                  </Button>
+                </Popover>
+              </div>
+            </div>
+          )}
+        </Editor>
+      </div>
+    );
+  }
+}
+
+export default AnswerEditor;
--- app/src/routes/scrapy/index.js
+++ app/src/routes/scrapy/index.js
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 import { Button, Card, Divider, Select, Input, Popover, Form } from "antd";
 import classNames from "classnames";
 import _ from "lodash";
-import { Editor } from "../../components";
-import { Inject, formatTime } from "../../utils";
+import { Inject } from "../../utils";
 import Preview from "./preview";
 import AnswerTable from "./answerTable";
+import AnswerEditor from "./answerEditor";
 import * as styles from "./index.module.scss";
-
-const { TextArea } = Input;
 
 @Form.create()
 @Inject(({ scrapyStore: model }) => ({
@@ -17,25 +15,12 @@ const { TextArea } = Input;
 class Scrapy extends Component {
   state = {
     selectOne: {},
-    editable: null,
     selectAnswerIds: [],
-    searchUrl: undefined,
-    showUserDownload: false
+    searchUrl: undefined
   };
 
   componentDidMount() {
     this.getAnswers();
-    window.ipc &&
-      window.ipc.on("scrapy.download-image", (e, arg) => {
-        if (!arg) return;
-        const images = document
-          .getElementById("answer-operation")
-          .querySelectorAll("img");
-        Array.prototype.map.call(images, item => {
-          const src = item.getAttribute("src");
-          item.setAttribute("src", `${src}?time=${Date.now()}`);
-        });
-      });
   }
 
   componentDidUpdate() {
@@ -73,19 +58,6 @@ class Scrapy extends Component {
     });
   };
 
-  updateAnswer = ({ answerId, ...rest }) => {
-    const {
-      model: { dispatch }
-    } = this.props;
-    dispatch({
-      type: "ipc-update-answer",
-      payload: {
-        answerId,
-        ...rest
-      }
-    });
-  };
-
   switchAnswer = ({ selectOne, editable }) => {
     this.setState({
       selectOne,
@@ -113,18 +85,10 @@ class Scrapy extends Component {
   };
 
   render() {
-    const {
-      selectOne,
-      editable,
-      selectAnswerIds,
-      searchUrl,
-      showUserDownload
-    } = this.state;
+    const { selectOne, selectAnswerIds, searchUrl } = this.state;
     const {
       model: { dispatch, answers = [], pagination }
     } = this.props;
-
-    const { getFieldDecorator } = this.props.form;
 
     return (
       <div className={classNames(styles.Scrapy, "page")}>
@@ -192,129 +156,7 @@ class Scrapy extends Component {
           </div>
         </div>
         <div className={styles.editor} id="answer-operation">
-          {selectOne.title && (
-            <div className={styles.contentHeader}>
-              <span className={styles.title}>{selectOne.title}</span>
-              <div className={styles.info}>
-                {selectOne.createTime && (
-                  <span className={styles.createTime}>
-                    {formatTime(selectOne.createTime)}
-                  </span>
-                )}
-                <span className={styles.author}>({selectOne.authorName})</span>
-              </div>
-            </div>
-          )}
-
-          <Editor
-            className={styles.editorContent}
-            content={selectOne.content}
-            editable={editable}
-          >
-            {editor => (
-              <div className={styles.utils}>
-                <div>
-                  <Button
-                    disabled={!editable}
-                    type="primary"
-                    onClick={() => {
-                      this.updateAnswer({
-                        answerId: selectOne.answerId,
-                        content: editor.txt.html()
-                      });
-                    }}
-                  >
-                    保存
-                  </Button>
-                  <Divider type="vertical" />
-                  <Button
-                    disabled={editable}
-                    type="primary"
-                    onClick={() => {
-                      this.setState({
-                        editable: true
-                      });
-                    }}
-                  >
-                    编辑
-                  </Button>
-                </div>
-                <div>
-                  <Popover
-                    visible={showUserDownload}
-                    getPopupContainer={() =>
-                      document.getElementById("user_download")
-                    }
-                    content={
-                      <div style={{ width: 400 }}>
-                        <Form
-                          onSubmit={e => {
-                            e.preventDefault();
-                            this.props.form.validateFields((err, values) => {
-                              if (!err) {
-                                const { filename, dataUrl } = values;
-                                console.log(values);
-                                // dispatch({
-                                //   type: "ipc-download-image",
-                                //   payload: {
-                                //     dataUrl,
-                                //     filename
-                                //   }
-                                // });
-                              }
-                            });
-                          }}
-                          className="login-form"
-                          {...{
-                            labelCol: {
-                              sm: { span: 5 }
-                            },
-                            wrapperCol: {
-                              sm: { span: 19 }
-                            }
-                          }}
-                        >
-                          <Form.Item label="filename">
-                            {getFieldDecorator("filename", {
-                              rules: [{ required: true, message: "required" }]
-                            })(<Input placeholder="filename" />)}
-                          </Form.Item>
-                          <Form.Item label="dataUrl">
-                            {getFieldDecorator("dataUrl", {
-                              rules: [{ required: true, message: "required" }]
-                            })(<TextArea placeholder="dataUrl" />)}
-                          </Form.Item>
-                          <Form.Item
-                            className={styles.nolabel}
-                            label={
-                              <span style={{ after: "unset" }}>&nbsp;</span>
-                            }
-                          >
-                            <Button type="primary" htmlType="submit">
-                              确认
-                            </Button>
-                          </Form.Item>
-                        </Form>
-                      </div>
-                    }
-                    title="手动添加图片"
-                    trigger="click"
-                  >
-                    <Button
-                      id="user_download"
-                      onClick={() => {
-                        this.setState({
-                          showUserDownload: !showUserDownload
-                        });
-                      }}
-                    >
-                      手动下载图片
-                    </Button>
-                  </Popover>
-                </div>
-              </div>
-            )}
-          </Editor>
+          <AnswerEditor selectOne={selectOne} />
           {selectOne.content && (
             <div className={styles.preview}>
               <Preview content={selectOne.content} />
