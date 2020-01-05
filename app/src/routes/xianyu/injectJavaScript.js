7fafa7157d04d664fbe3b5ab0afaaf28a8290825
weixiaoyi <2278095567@qq.com>
2020-01-05 16:21:18 +0800

    yarnrc文件

--- app/src/routes/xianyu/injectJavaScript.js
+++ app/src/routes/xianyu/injectJavaScript.js
@@ -5,16 +5,22 @@ const injectJavaScript = () => {
   div.innerHTML = `test`;
   document.body.appendChild(div);
   const url = window.location.href;
-  const seller = document
-    .getElementById("J_SellerInfo")
-    .getElementsByClassName("wangwang")[0].innerText;
+  const sellerInfo = document.getElementById("J_SellerInfo");
+  const wangwang = sellerInfo.getElementsByClassName("wangwang")[0].innerText;
+  const userVertify = sellerInfo.getElementsByClassName("user-verify")[0].title;
+  const vip = sellerInfo.getElementsByClassName("vip-level")[0].title;
+  // const sex = sellerInfo.getElementsByClassName("basic-info")[0].get;
   window.ipc.send("ipc", {
     from: "app.wins.main.render",
     data: {
       type: "xianyu.get-product",
       message: {
         url,
-        seller
+        sellerInfo: {
+          wangwang,
+          userVertify,
+          vip
+        }
       }
     }
   });
